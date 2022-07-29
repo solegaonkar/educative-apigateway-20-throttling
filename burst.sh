961f1932450fb8e66ad603deab8db35c12c32c7e
Vikas Solegaonkar <vikas.solegaonkar@gmail.com>
2022-07-29 12:22:23 +0000

    Update

---
+++ burst.sh
@@ -0,0 +1,3 @@
+apiId=`aws cloudformation list-stack-resources --stack-name EducativeCourseApiGateway | jq -r ".StackResourceSummaries[2].PhysicalResourceId"`
+url="https://${apiId}.execute-api.us-east-1.amazonaws.com/v1/hello"    
+for i in {1..100}; do curl --location --request POST $url --header 'Content-Type: application/json' --data-raw '{ "message": "Hello World" }' & done
